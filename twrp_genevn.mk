220150bcff10c3dfe0d605f28da7eac6d6bd7087
015stray91 <133708414+015stray91@users.noreply.github.com>
2026-07-10 08:35:11 -0500

    Update and rename twrp_yume.mk to twrp_genevn.mk

--- twrp_genevn.mk
+++ twrp_genevn.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP Device Tree Generator
-# Copyright (C) 2019-Present A-Team Digital Solutions
-# Copyright (C) 2024 sosRR
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
@@ -16,21 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Inherit some common TWRP/OFox stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from genevn device
-$(call inherit-product, device/motorola/yume/device.mk)
+# Inherit from the native factory device path configuration
+$(call inherit-product, device/motorola/genevn/device.mk)
 
-PRODUCT_DEVICE := yume
-PRODUCT_NAME := twrp_yume
-PRODUCT_BRAND := Moto_G
-PRODUCT_MODEL := XT2315
-PRODUCT_MANUFACTURER := Motorola
+PRODUCT_DEVICE := genevn
+PRODUCT_NAME := twrp_genevn
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola moto g stylus 5g (2023)
+PRODUCT_MANUFACTURER := motorola
 PRODUCT_RELEASE_NAME := Moto G Stylus 5G 2023
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
+# Hardcoded for your exact live Android 14 System configuration
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=genevn \
-    PRIVATE_BUILD_DESC="genevn-user 12 T1TGN33.60-114 7117f-b6786 release-keys"
+    TARGET_PRODUCT=genevn_g \
+    PRIVATE_BUILD_DESC="genevn_g-user 14 U1TGNS34.42-86-2-32 release-keys"
 
-BUILD_FINGERPRINT := motorola/genevn/genevn:12/T1TGN33.60-114/7117f-b6786:user/release-keys
-
+BUILD_FINGERPRINT := motorola/genevn_g/genevn:14/U1TGNS34.42-86-2-32:user/release-keys
